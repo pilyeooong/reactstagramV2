6bbbb5f2c9ce7e3ad9d845d15a375de34248e17f
PilYeong <pilyeooong@gmail.com>
2020-04-23 12:58:09 +0900

    ADD: profile 페이지, 이미지 업로드를 위한 base64 코드 추가

--- frontend/src/components/Post.js
+++ frontend/src/components/Post.js
@@ -1,13 +1,42 @@
 import React from "react";
 import "./Post.scss";
+import { Avatar, Card } from "antd";
 
 
 function Post({ post }) {
+  const { photo, caption, author : { avatar_url, username }, location } = post;
   return (
     <div className="post">
-      <img src={post.photo} alt="" style={{ width : "680px"}}/>
-      <div>{post.author.username}</div>
-      <div>{post.caption}</div>
+      <Card
+        hoverable
+        cover={<img src={photo} alt={caption} />}
+        // actions={[
+        //   is_like ? (
+        //     <HeartFilled onClick={() => handleLike({ post, isLike: false })} />
+        //   ) : (
+        //     <HeartOutlined onClick={() => handleLike({ post, isLike: true })} />
+        //   ),
+        // ]}
+        style={{ marginBottom: "1rem" }}
+      >
+        <Card.Meta
+          avatar={
+            <Avatar
+              size="large"
+              icon={
+                <img
+                  src={"http://localhost:8000" + avatar_url}
+                  alt={username}
+                />
+              }
+            />
+          }
+          title={location}
+          description={caption}
+          style={{ marginBotton: "20px" }}
+        />
+        {/* <CommentList post={post} /> */}
+      </Card>
     </div>
   )
 }
